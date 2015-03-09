539254553ab194132312c3130d45b134a453bd68
Frederik Hanghøj Iversen <me@fredefox.eu>
2015-03-09 16:50:18 +0100

    Implement affine linear model

--- solution.py
+++ solution.py
@@ -29,25 +29,19 @@ def lin_reg(S):
     the lecture notes but it's a bit difficult to read because
     the x-vectors are in bold differing them from the y's that are
     just values and are not emphasised """
-    y = matrix(list(map(lambda s: s[1], S)))
+    y = transpose(matrix(list(map(lambda s: s[1], S))))
     # `x` is a matrix
     x = matrix(list(map(lambda s: s[0], S)))
     z = ones((x.shape[0], 1), int)
     X = append(x, z, axis=1)
-    # I tried with the data and
-    #   (X^T * X)^(-1)
-    # was not defined, however
-    #   (X * X^T)^(-1)
-    # was, that is, transpose in different order.
     X_t = transpose(X)
     t = inv(dot(X_t, X))
     w = dot(dot(t, X_t), y)
-    # TODO: The last element of `w` is `b` and only everything up until that
-    # is the `w` mentioned in the lecture notes.
-    # pesudo-code:
-    #   w = [n+1]
-    #   w = w[0:n]
-    return lambda x: dot(w, x)
+    n, _ = w.shape
+    b = w[n-1, 0]
+    w = w[0:n-1, 0]
+    w_t = transpose(w)
+    return lambda x: dot(w_t, x) + b
 
 if __name__ == "__main__":
     # Question 1
@@ -62,6 +56,6 @@ if __name__ == "__main__":
     s_var = variance(y_values)
     print("Sample variance: {}".format(s_var))
     xs = redshift_x()
-    zipd = zip(xs, y_values)
+    zipd = list(zip(xs, y_values))
     l_reg = lin_reg(zipd)
     print("Linear regression: {}".format(l_reg))
